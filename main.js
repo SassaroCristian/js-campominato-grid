2ea8c3bdcda77cc69b87f69ea572ef20d5e3e85a
Zazzaro <sassaro01cristian@gmail.com>
2023-11-07 21:43:53 +0100

    added different difficulties

--- main.js
+++ main.js
@@ -1,9 +1,40 @@
-let numCelle = 99;
+
 let grid = document.getElementById("grid");
 let playBtn = document.getElementById("playBtn");
 let difficultySelector = document.getElementById("difficultySelector");
 
-function createCells () {
+function createCellsEasy () {
+    let numCelle = 99;
+    for (let i = 0; i <= numCelle; i++) {
+    const cella = document.createElement ("div")
+    cella.classList.add ("squareSmall");
+    cella.textContent = i + 1;
+    grid.appendChild (cella);
+    
+    cella.addEventListener ("click", function () {
+    cella.classList.toggle("highlight");
+    console.log(cella.textContent = i + 1)
+        });
+    }
+}
+
+function createCellsMedium () {
+    let numCelle = 79;
+    for (let i = 0; i <= numCelle; i++) {
+    const cella = document.createElement ("div")
+    cella.classList.add ("squareMedium");
+    cella.textContent = i + 1;
+    grid.appendChild (cella);
+    
+    cella.addEventListener ("click", function () {
+    cella.classList.toggle("highlight");
+    console.log(cella.textContent = i + 1)
+        });
+    }
+}
+
+function createCellsHard () {
+    let numCelle = 49;
     for (let i = 0; i <= numCelle; i++) {
     const cella = document.createElement ("div")
     cella.classList.add ("square");
@@ -17,4 +48,8 @@ function createCells () {
     }
 }
 
-playBtn.addEventListener("click", createCells);
+// function difficulty (){
+//     if 
+// }
+
+playBtn.addEventListener("click", createCellsHard);
